e7bf93705960169d3e09f4fb2d6e7fc80e526755
Ricardo Lopez Peña <ricardoslopezs2001@gmail.com>
2024-10-15 18:07:00 +0200

    Java, ejercicio 8. Completado

--- src/main/java/com/campusdual/classroom/Exercise08.java
+++ src/main/java/com/campusdual/classroom/Exercise08.java
@@ -1,8 +1,16 @@
 package com.campusdual.classroom;
 
+import java.util.Scanner;
+
 public class Exercise08 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Escribe el numero x: ");
+        int num = scanner.nextInt();
+        whileLoop(num);
+
+
 
     }
 
@@ -12,6 +20,12 @@ public class Exercise08 {
     // X+1 < Y. El próximo ciclo valdrá: X+2
     // etc.
     public static void whileLoop(int num) {
+        int x = 0;
 
+        while (num > x) {
+            System.out.println(x + " < " + num + ". El proximo ciclo valdra: " + (x + 1));
+            x = x + 1;
+//num+" < "+n1+". El proximo ciclo valdra: "+(num+1)
+        }
     }
 }
